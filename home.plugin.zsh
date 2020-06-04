94bda854c15d26a21fd9cddba8c9f9d5856e4158
Adam Stankiewicz <sheerun@sher.pl>
2020-06-04 13:49:45 +0200

    Use mac keybindings

--- home.plugin.zsh
+++ home.plugin.zsh
@@ -2,9 +2,8 @@
 PROMPT="%F{000}%(?.%K{148}.%K{red}) %(?.⌂.✘) %F{249}%K{236} %1~ %k%f "
 unset RPROMPT
 
-# handle ctrl-a and ctrl-e
-bindkey '^a' beginning-of-line
-bindkey '^e' end-of-line
+# use emacs key-bindings
+bindkey -e
 
 # make it easier to remove words with ctrl-w
 export WORDCHARS='*?[]~&;!$%^<>'
